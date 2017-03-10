6637c5a2e2a1b55a5149912502d8cadf0c8e0db6
westondombroski <westondombroski@gmail.com>
2017-03-10 15:53:35 -0800

    added POST new user route/functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,15 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def create
+    @user = User.create(user_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
- root to: "users#index"
+  root to: "users#index"
 
-  resources :users
- # get '/users/new', to: 'users#new', as: 'new_user'
- # post '/users/', to: 'users#create'
+  get '/users/new', to: 'users#new', as: 'new_user'
+  post '/users', to: 'users#create'
 end
